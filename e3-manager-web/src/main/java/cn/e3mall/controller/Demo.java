9b39cf76ac13fffd93bde42b2d6900e1fbf85dc9
Listenerwen <Listenerwen@163.com>
2017-06-30 20:20:36 +0800

    Update Demo.java

--- e3-manager-web/src/main/java/cn/e3mall/controller/Demo.java
+++ e3-manager-web/src/main/java/cn/e3mall/controller/Demo.java
@@ -2,7 +2,7 @@ package cn.e3mall.controller;
 
 public class Demo {
 	public static void main(String[] args) {
-		
+		int i=998;
 		//kkkk
 		System.out.println("hello");
 		System.out.println(1234);
