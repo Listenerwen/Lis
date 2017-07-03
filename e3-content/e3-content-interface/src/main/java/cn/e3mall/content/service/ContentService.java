b99fc630e75b0416550ae58e13a72fd2c3c65b50
lew <Listenerwen@163.com>
2017-07-03 20:33:03 +0800

    后台内容管理模块添加

---
+++ e3-content/e3-content-interface/src/main/java/cn/e3mall/content/service/ContentService.java
@@ -0,0 +1,11 @@
+package cn.e3mall.content.service;
+
+import cn.e3mall.common.pojo.DataGridResult;
+import cn.e3mall.common.pojo.E3Result;
+import cn.e3mall.pojo.TbContent;
+
+public interface ContentService {
+
+	DataGridResult getConentList(long categoryId, int page, int rows);
+	E3Result addContent(TbContent content);
+}
